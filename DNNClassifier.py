443a7a4be58a8fd06bcdacb881c3bda44b5446cc
byehia <beshoyatwork@hotmail.com>
2017-12-24 12:06:33 -0800

    Increase accuracy to around 93%

--- DNNClassifier.py
+++ DNNClassifier.py
@@ -358,13 +358,13 @@ def train_top_model():
     model.add(Flatten(input_shape=train_data.shape[1:]))
     model.add(Dense(256, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='sigmoid'))
+    model.add(Dense(num_classes, activation='softmax'))
 
-    model.compile(optimizer='rmsprop',
+    model.compile(optimizer='adam',
                   loss='categorical_crossentropy', metrics=['accuracy'])
 
     model.fit(train_data, train_labels,
-              epochs=5,
+              epochs=100,
               batch_size=16,
               validation_data=(validation_data, validation_labels))
     # model.save_weights(top_model_weights_path)
